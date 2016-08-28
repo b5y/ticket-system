0c627c65cb97c7b030d198691c2b7a0e1ac69cf2
Mehti Musayev <mehty.musaev@gmail.com>
2016-08-28 21:02:02 +0300

    Fix some bugs.

--- ticket_system.py
+++ ticket_system.py
@@ -21,61 +21,49 @@ def connect_db():
                                 user='postgres',
                                 password='postgres',
                                 host='localhost')
-    except TypeError:
-        logger.exception("Cannot connect to the {0} database".format(DB_NAME))
-
-
-def verify_id(_id=int):
-    # remove after setting auto increment in database
-    if not 1 <= _id <= 2147483647:
-        logger.exception('Ticket number {0} is out of bound'.format(_id))
-        raise ValueError()
-    return True
+    except TypeError as t_e:
+        logger.exception("Cannot connect to the {0} database".format(DB_NAME), t_e)
 
 
 def verify_email_address(email=basestring):
-    pattern = re.compile(
-        ur'^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|([-a-z0-9!#$%&@\s\'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$')
+    pattern = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
     if re.search(pattern, email):
         return True
     else:
-        logger.exception('Email address {0} is invalid'.format(email))
-        raise ValueError()
-    return False
+        raise ValueError('Email address {0} is invalid'.format(email))
 
 
 @app.route('/')
-def create_ticket(ticket_id=int, subject=basestring, text=basestring,
+def create_ticket(subject=basestring, text=basestring,
                   email=basestring, state=basestring):
     conn = connect_db()
     date_time = datetime.datetime
     if not conn:
         return False
     cur = conn.cursor()
-    try:
-        if verify_id(ticket_id) and verify_email_address(email):
-            cur.execute('INSERT INTO tickets('
-                        'ticket_id,'
-                        'create_date,'
-                        'change_date,'
-                        'subject,'
-                        'text,'
-                        'email,'
-                        'state)'
-                        'VALUES ({0}, {1}, {2}, {3}, {4}, {5}, {6});'
-                        .format(ticket_id,
-                                date_time,
-                                date_time,
-                                subject,
-                                text,
-                                email,
-                                state))
+    if verify_email_address(email):
+        try:
+            ticket_id = cur.execute('INSERT INTO tickets('
+                                    'create_date,'
+                                    'change_date,'
+                                    'subject,'
+                                    'text,'
+                                    'email,'
+                                    'state)'
+                                    'VALUES ({0}, {1}, {2}, {3}, {4}, {5}, {6}) '
+                                    'RETURNING id;'
+                                    .format(date_time,
+                                            date_time,
+                                            subject,
+                                            text,
+                                            email,
+                                            state))
             if cur.fetchone():
                 cache.set(ticket_id, cur.fetchone(), timeout=5 * 30)
             return True
-    except IOError:
-        logger.exception('Error creating ticket {0} with email address {1}'
-                         .format(ticket_id, email))
+        except IOError as io_e:
+            logger.exception('Error creating new data with email address {0}'
+                             .format(email), io_e)
     return False
 
 
@@ -87,39 +75,38 @@ def change_state(ticket_id=int, new_state=basestring):
     cur = conn.cursor()
     date_time = datetime.datetime
     try:
-        if verify_id(ticket_id):
-            cur.execute('UPDATE tickets'
-                        'SET state={0}, change_date={1} WHERE ticket_id={2};'
-                        .format(new_state, date_time, ticket_id))
-            if cur.fetchone():
-                cache.set(ticket_id, cur.fetchone(), timeout=5 * 30)
-            return True
+        cur.execute('UPDATE tickets'
+                    'SET state={0}, change_date={1} WHERE ticket_id={2};'
+                    .format(new_state, date_time, ticket_id))
+        if cur.fetchone():
+            cache.set(ticket_id, cur.fetchone(), timeout=5 * 30)
+        return True
     except IOError:
         logger.exception('Error changing state')
     return False
 
 
 @app.route('/')
-def add_comment(comment_id=int, ticket_id=int, create_date=basestring,
+def add_comment(ticket_id=int, create_date=basestring,
                 email=basestring, text=basestring):
     conn = connect_db()
     if not conn:
         return False
     cur = conn.cursor()
-    try:
-        if (verify_id(comment_id) and verify_id(ticket_id) and
-                verify_email_address(email)):
+    if verify_email_address(email):
+        try:
             cur.execute('INSERT INTO comments('
-                        'comment_id,'
                         'ticket_id,'
                         'create_date,'
                         'email,'
                         'text)'
-                        'VALUES ({0}, {1}, {2}, {3}, {4})'
-                        .format(comment_id, ticket_id, create_date, email, text))
+                        'VALUES ({0}, {1}, {2}, {3}, {4});'
+                        .format(ticket_id, create_date, email, text))
             return True
-    except IOError:
-        logger.exception('Error add comment')
+        except IOError:
+            logger.exception('Error adding comment {0}'.format(text))
+    else:
+        raise ValueError('Invalid email address')
     return False
 
 
@@ -129,17 +116,16 @@ def get_ticket(ticket_id=int):
     if not conn:
         return None
     cur = conn.cursor()
-    try:
-        if verify_id(ticket_id):
-            if cache.get(ticket_id):
-                return cache.get(ticket_id)
-            else:
-                cur.execute('SELECT * FROM tickets WHERE ticket_id='.format(ticket_id))
-                if cur.fetchone():
-                    cache.set(ticket_id, cur.fetchone(), timeout=5 * 30)
-                    return cur.fetchone()
-    except IOError:
-        logger.exception('Error getting ticket')
+    if cache.get(ticket_id):
+        return cache.get(ticket_id)
+    else:
+        try:
+            cur.execute('SELECT * FROM tickets WHERE ticket_id={0};'.format(ticket_id))
+            if cur.fetchone():
+                cache.set(ticket_id, cur.fetchone(), timeout=5 * 30)
+                return cur.fetchone()
+        except IOError:
+            logger.exception('Error getting ticket {0}'.format(ticket_id))
     return None
 
 
