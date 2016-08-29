dcd46d6e4f55ade886bbabe379dde6ad4b9e980d
Mehti Musayev <mehty.musaev@gmail.com>
2016-08-30 00:11:19 +0300

    Add package finder and version for setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='ticket-system',
-    packages=['ticket-system'],
+    version='0.0.1',
     include_package_data=True,
     install_requires=[
         'flask >= 0.11.1',
@@ -10,6 +10,7 @@ setup(
         'uwsgi >= 2.0.13.1',
         'pylibmc >= 1.5.1',
     ],
+    packages=find_packages(),
     setup_requires=[
         'pytest-runner',
     ],
