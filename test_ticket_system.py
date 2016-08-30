340da68517515210ad357f9aa458ab0c05f674ee
Mehti Musayev <mehty.musaev@gmail.com>
2016-08-30 12:12:43 +0300

    Add test blanks.

--- test_ticket_system.py
+++ test_ticket_system.py
@@ -1,9 +1,27 @@
 import os
 import unittest
+import psycopg2
+from app import app, connect_db, create_ticket, change_state, add_comment, get_ticket
 
 
 class TestTicketSystem(unittest.TestCase):
-    pass
+    def setUp(self):
+        self._app = app
+
+    def test_connect_db(self):
+        assert connect_db() is not None
+
+    def test_create_ticket(self):
+        pass
+
+    def test_change_state(self):
+        pass
+
+    def test_add_comment(self):
+        pass
+
+    def test_get_ticket(self):
+        pass
 
 
 if __name__ == '__main__':
