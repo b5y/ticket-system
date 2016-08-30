6a49661568b61cbff911934c7f858bf94c85da6f
Mehti Musayev <mehty.musaev@gmail.com>
2016-08-30 14:26:27 +0300

    Set ids type as serial. Update dropping.

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 -- read before running server
 
-DROP TABLE IF EXISTS tickets;
+DROP TABLE IF EXISTS tickets CASCADE;
 
 CREATE TABLE IF NOT EXISTS tickets
 (
@@ -14,14 +14,17 @@ CREATE TABLE IF NOT EXISTS tickets
 );
 
 
-DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS comments CASCADE;
 
 CREATE TABLE IF NOT EXISTS comments
 (
   id          SERIAL PRIMARY KEY,
-  ticket_id   INTEGER NOT NULL,
+  ticket_id   SERIAL,
   create_date DATE,
   email       VARCHAR(254),
   text        TEXT,
   FOREIGN KEY (ticket_id) REFERENCES tickets (id)
 );
+
+GRANT ALL PRIVILEGES ON TABLE tickets TO postgres;
+GRANT ALL PRIVILEGES ON TABLE comments TO postgres;
