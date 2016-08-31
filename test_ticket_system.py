9e0d0c01c15fad2a039b27c1b8297ee5c3d42a59
Mehti Musayev <mehty.musaev@gmail.com>
2016-08-31 10:14:03 +0300

    Add more tests.

--- test_ticket_system.py
+++ test_ticket_system.py
@@ -18,12 +18,19 @@ class TestTicketSystem(unittest.TestCase):
                                    text='add new functionality',
                                    email='example@example.ru',
                                    state='In Progress')
+        incorrect_new_ticket = create_ticket(subject='feature',
+                                             text='add new feature',
+                                             email='@@@#@example.com',
+                                             state='In Progress')
 
         assert new_ticket is True
+        assert incorrect_new_ticket is False
 
     def test_change_state(self):
         new_state = change_state(ticket_id=1, new_state='Done')
+        incorrect_new_state = change_state(ticket_id=100, new_state='Done')
         assert new_state is True
+        assert incorrect_new_state is False
 
     def test_add_comment(self):
         create_ticket(subject='functionality',
@@ -33,11 +40,17 @@ class TestTicketSystem(unittest.TestCase):
         new_comment = add_comment(ticket_id=1,
                                   email='comment@example.ru',
                                   text='new comment')
+        incorrect_new_comment = add_comment(ticket_id=100,
+                                            email='comment@example.com',
+                                            text='new comment')
         assert new_comment is True
+        assert incorrect_new_comment is False
 
     def test_get_ticket(self):
         get_ticket_ = get_ticket(ticket_id=1)
+        incorrect_ticket = get_ticket(ticket_id=100)
         assert get_ticket_ is not None
+        assert incorrect_ticket is None
 
 
 if __name__ == '__main__':
