2ff2e17e15967280f62ae027a2e987e77ef04170
Mehti Musayev <mehty.musaev@gmail.com>
2016-08-31 12:43:10 +0300

    Fix tests.

--- test_ticket_system.py
+++ test_ticket_system.py
@@ -1,17 +1,14 @@
 import unittest
-from app import app, connect_db, verify_email_address, create_ticket, \
+from app import connect_db, verify_email_address, create_ticket, \
     change_state, add_comment, get_ticket
 
 
 class TestTicketSystem(unittest.TestCase):
-    def setUp(self):
-        self._app = app
-
     def test_connect_db(self):
-        assert connect_db() is not None
+        self.assertIsNot(connect_db(), None)
 
     def test_verify_email_address(self):
-        assert verify_email_address('example@example.com') is True
+        self.assertTrue(verify_email_address('example@example.com'))
 
     def test_create_ticket(self):
         new_ticket = create_ticket(subject='functionality',
@@ -27,15 +24,19 @@ class TestTicketSystem(unittest.TestCase):
                                                email='example@example.ru',
                                                state='In Progress')
 
-        assert new_ticket is True
-        assert incorrect_email_ticket is False
-        assert incorrect_state_ticket is False
+        self.assertTrue(new_ticket)
+        self.assertFalse(incorrect_email_ticket)
+        self.assertFalse(incorrect_state_ticket)
 
     def test_change_state(self):
-        new_state = change_state(ticket_id=1, new_state='answered')
+        create_ticket(subject='functionality',
+                      text='add new functionality',
+                      email='example@example.ru',
+                      state='Open')
+        new_state = change_state(ticket_id=2, new_state='answered')
         incorrect_new_state = change_state(ticket_id=100, new_state='Open')
-        assert new_state is True
-        assert incorrect_new_state is False
+        self.assertTrue(new_state)
+        self.assertFalse(incorrect_new_state)
 
     def test_add_comment(self):
         create_ticket(subject='functionality',
@@ -45,22 +46,22 @@ class TestTicketSystem(unittest.TestCase):
         new_comment = add_comment(ticket_id=1,
                                   email='comment@example.ru',
                                   text='new comment')
-        assert new_comment is True
+        self.assertTrue(new_comment)
         incorrect_new_comment = add_comment(ticket_id=100,
                                             email='comment@example.com',
                                             text='new comment')
-        assert incorrect_new_comment is False
+        self.assertFalse(incorrect_new_comment)
         change_state(ticket_id=1, new_state='Closed')
         add_comment_to_closed_ticket = add_comment(ticket_id=1,
                                                    email='comment@example.com',
                                                    text='new comment')
-        assert add_comment_to_closed_ticket is False
+        self.assertFalse(add_comment_to_closed_ticket)
 
     def test_get_ticket(self):
         get_ticket_ = get_ticket(ticket_id=1)
         incorrect_ticket = get_ticket(ticket_id=100)
-        assert get_ticket_ is not None
-        assert incorrect_ticket is None
+        self.assertIsNot(get_ticket_, None)
+        self.assertIs(incorrect_ticket, None)
 
 
 if __name__ == '__main__':
