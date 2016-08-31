78f9bb7186889b4542dfc76dd2be6ca9f7f62e84
Mehti Musayev <mehty.musaev@gmail.com>
2016-08-31 20:20:23 +0300

    Clear code. Remove useless comments.

--- run_server.sh
+++ run_server.sh
@@ -11,19 +11,25 @@ pip install -e .
 psql -c 'create database ticket_system;' -U postgres
 export PGPASSWORD="postgres"
 psql -f schema.sql -U postgres -d ticket_system
+# Add uWSGI to PPA:
 sudo add-apt-repository ppa:uwsgi/release
 sudo apt-get update && sudo apt-get -y upgrade
+# Install required packages:
 sudo apt-get install uwsgi
-sudo mkdir -p /var/log/uwsgi
-sudo mkdir -p /etc/uwsgi/apps-available
-sudo mkdir -p /etc/uwsgi/apps-enabled
 sudo apt-get install memcached
 sudo apt-get install libmemcached-dev
+# Run uwsgi to check that everything is OK
+(sleep 5; echo uwsgi --socket 127.0.0.1:3031 --wsgi-file app.py --callable app --master) &
 memcached -u memcached -d -m 30 -l 127.0.0.1 -p 11211
-sudo cp uwsgi.conf /etc/init/uwsgi.conf
+# Creating folders relating to running uwsgi:
+sudo mkdir /var/run/flask-uwsgi
+sudo chown www-data:www-data /var/run/flask-uwsgi
+sudo mkdir /var/log/flask-uwsgi
+sudo chown www-data:www-data /var/log/flask-uwsgi
+sudo mkdir /etc/flask-uwsgi
+sudo cp flask-uwsgi.conf /etc/init/flask-uwsgi.conf
+sudo cp flask-uwsgi.ini /etc/flask-uwsgi/flask-uwsgi.ini
 sudo service uwsgi start
-sudo cp flaskapp.ini /etc/uwsgi/apps-available/flaskapp.ini
-sudo ln -s /etc/uwsgi/apps-available/flaskapp.ini /etc/uwsgi/apps-enabled/flaskapp.ini
 py.test test_ticket_system.py
 sudo service uwsgi stop
 
